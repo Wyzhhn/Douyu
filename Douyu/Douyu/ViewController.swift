3b1610e349462ff1eeadc0a7efd60ea34cbcd4b9
MGX72 <mgx72@xinmaidemacbook-pro.local>
2016-09-26 09:49:05 +0800

    初始化app

---
+++ Douyu/Douyu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Douyu
+//
+//  Created by xinmai on 16/9/26.
+//  Copyright © 2016年 xinmai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
